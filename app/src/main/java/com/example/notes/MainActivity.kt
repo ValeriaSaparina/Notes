94a74c0a8db3f0c2ccd59300f558d93ef4c4c2ff
Valeria Saparina <kzn.2003valeria@gmail.com>
2024-12-05 02:27:25 +0300

    init commit

---
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.notes.ui.FolderItemUiModel
+import com.example.notes.ui.base.component.ExpandableList
+import com.example.notes.ui.base.data.SectionData
+import com.example.notes.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(name = "Hello world!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotesTheme {
+        Greeting("Android")
+    }
+}
